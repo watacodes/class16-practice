1affe0844c4a777225234a6fb0466acf7b2796ae
Wataru Hori <Bireleysaddiction@gmail.com>
2022-07-14 23:22:47 +0900

    Practiced switch statement and arrow functions by making a Rock Paper Scissors game

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
-# class16-practice
+// JavaScript Playground
+
+// Test out Number() command
+
+const input = prompt('Enter a number: ');
+const inputToNumber = Number(input);
+
+const input2 = prompt('Enter a string: ');
+const stringToNumber = Number(input2);
+
+// Testing out input buttons and switch statement
+
+const buttons = document.querySelectorAll('.buttons');
+const placeholder = document.getElementById('placeholder');
+
+const trigger = (e) => {
+    let getValue = e.target.value;
+    let compPlay = 'Scissors';
+
+    switch(getValue) {
+        case "Rock":
+            placeholder.innerText = `Your chose ${getValue}! CPU selected ${compPlay}, You won!`;
+            break;
+        case "Paper":
+            placeholder.innerText = `You chose ${getValue}! CPU selected ${compPlay}, You lost!`;
+            break;
+        case "Scissors":
+            placeholder.innerText = `You chose ${getValue}! CPU selected ${compPlay}, Play again!`;
+            break;
+        default:
+            console.log('User has selected none');
+    }
+};
+
+Array.from(buttons).forEach(btn => btn.addEventListener('click', trigger));
+
