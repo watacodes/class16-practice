303b6c24cec8d62de22455ea034ea4c8b0c9bc40
Wataru Hori <Bireleysaddiction@gmail.com>
2022-07-20 23:33:49 +0900

    Codewards challenge 8k

--- main.js
+++ main.js
@@ -1,248 +1,40 @@
-// JavaScript Playground
-
-// Test out Number() command
-
-// const input = prompt('Enter a number: ');
-// const inputToNumber = Number(input);
-
-// const input2 = prompt('Enter a string: ');
-// const stringToNumber = Number(input2);
-
-// Testing out input buttons and switch statement
-
-const buttons = document.querySelectorAll('.buttons');
 const placeholder = document.getElementById('placeholder');
 
-const trigger = (e) => {
-    let getValue = e.target.value;
-    let compPlay = 'Scissors';
+///////////////////////////////////////////////////////////
 
-    switch(getValue) {
-        case "Rock":
-            placeholder.innerText = `You chose ${getValue}! CPU selected ${compPlay}, You won!`;
-            break;
-        case "Paper":
-            placeholder.innerText = `You chose ${getValue}! CPU selected ${compPlay}, You lost!`;
-            break;
-        case "Scissors":
-            placeholder.innerText = `You chose ${getValue}! CPU selected ${compPlay}, Play again!`;
-            break;
-        default:
-            console.log('User has selected none');
-    }
-};
+// Codewars question (https://www.codewars.com/kata/5601409514fc93442500010b/train/javascript)
+// Title: How good are you really?
+// Result: Passed
 
-Array.from(buttons).forEach(btn => btn.addEventListener('click', trigger));
-
-// Chessboard
-
+// Question detail:
 /* 
-Write a program that creates a string that represents an 8×8 grid, 
-using newline characters to separate lines.
-At each position of the grid there is either a space or a "#" character.
-The characters should form a chessboard.
 
-Passing this string to console.log should show something like this:
+There was a test in your class and you passed it. Congratulations!
+But you're an ambitious person. You want to know if you're better than the average student in your class.
 
- # # # #
-# # # # 
- # # # #
-# # # # 
- # # # #
-# # # # 
- # # # #
-# # # #
+You receive an array with your peers' test scores. Now calculate the average and compare your score!
 
-When you have a program that generates this pattern, 
-define a binding size = 8 and change the program so that it works for any size, 
-outputting a grid of the given width and height.
+Return True if you're better, else False!
 
-1. The odd rows should start with a blankspace first.
-
-2. The even rows should start with a hash symbol.
-
-
+Note:
+Your points are not included in the array of your class's points. For calculating the average point you    may add your point to the given array!
 
 */
 
-let space = "";
-for (let row = 1; row < 9; row++) {
-    for (let col = 1; col < 9; col++) {
-        if ((row + col) % 2 == 0) {
-            space += " ";
-        } else {
-            space += "#";
-        }
-    }
-    space += "\n";
+function betterThanAverage(classPoints, yourPoints) {
+    // Your code here
+    // Add all the classPoints, which will be given in an array form.
+    // After that, divide it by the number of values in the array.
+    // Store the value in a seperate variable
+    // If newVar < yourPoints, return true : return false.
+    
+    let average = 0;
+    let sum = 0;
+    classPoints.map(x => sum += x);
+    average = sum / classPoints.length;
+    let result = (yourPoints > average) ? true : false;
+    placeholder.innerText = `Your test result is: ${result}`;
+    return result;
 }
 
-console.log(space);
-
-// Find even number
-
-const isEven = (num) => {
-    if (num < 0) {
-        return false;
-    } else if (num === 0 || num % 2 === 0) {
-        return true;
-    } else {
-         return false; 
-    }
-}
-
-console.log(isEven(50));
-console.log(isEven(75));
-console.log(isEven(-1));
-
-// Find minimum number without using Math.min() method
-
-const min = (a, b) => a > b ? b : a;
-
-console.log(min(9, 2));
-console.log(min('Zebra', 'monkey'));
-
-console.log('Z'.charCodeAt(0));
-console.log('monkey'.charCodeAt(0));
-
-// Count how many "B" letters in a string
-
-const countBs = (str) => {
-    let b = 0;
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === "B") {
-            b++;
-        }
-    }
-    return b;
-};
-
-console.log(countBs('BeeBeeb'));
-console.log(countBs('beepBo'));
-
-// Count specific letters given by user as a second param 
-
-const countChars = (str, letter) => {
-    let counter = 0;
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === letter) {
-            counter++;
-        }
-    }
-    return counter;
-};
-
-console.log(countChars('Riceball Kingdom', 'l'));
-console.log(countChars('I am AaaAAA', "A"));
-
-// checkboard
-
-let gridSize = 8;
-let chess = "";
-function chessBoard() {
-  for (let row = 0; row < gridSize; row++) {
-    for (let col = 0; col < gridSize; col++) {
-      if ((row + col) % 2 === 0) {
-        chess += " ";
-      } else {
-        chess += "#";
-      }
-    }
-    chess += "\n";
-  }
-  console.log(chess);
-}
-
-chessBoard();
-
-// triangle
-
-    // line 1 => one hash sign
-    // linebreak
-    // line 2 => two hash signs
-    // linebreak
-    // continue till 7
-for (let triangle = "#"; triangle.length < 8; triangle += "#") {
-    console.log(triangle);
-}
-
-// countBs (revision)
-
-const countLetters = (str, letter) => {
-    let count = 0;
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === letter) {
-            count++;
-        }
-    }
-    return count;
-}
-
-console.log(countLetters('Meee', 'e'));
-
-// make triangle (revision)
-
-function makeTri() {
-    for (let i = "#"; i.length < 8; i += "#") {
-    console.log(i);
-    }
-};
-
-makeTri();
-
-// chessboard (revision)
-// 1-1 blankspace,
-// 1-2 hash sign, => if col + row is even, then there'll be a blankspace.
-
-const makeChessBoard = () => {
-    // set the grid size to 8
-    let grid = 8;
-    // Declare a variable that stores the hash signs
-    let board = "";
-    // Write two for loops for rows and cols
-    for (let i = 0; i < grid; i++) {
-        for (let j = 0; j < grid; j++) {
-            if ((i + j) % 2 === 0) {
-                board += " ";
-            } else {
-                board += "#";
-            }
-        }
-        board += "\n";
-    }
-    return board;
-}
-
-console.log(makeChessBoard());
-
-// Array manipulation practice
-
-const groceryList = ['Banana', 'Egg', 'Chocolate', 'Eggplant', 'Rice', 'Tea', 'Rice Cracker'];
-
-// Add an item to the end of the grocery list
-
-groceryList.push('Apple');
-console.log(groceryList);
-
-// Delete the first item of the grocery list
-
-groceryList.shift();
-console.log(groceryList);
-
-// Delete the last item of the grocery list
-
-groceryList.pop();
-console.log(groceryList);
-
-// Add 'Bread' and 'Salmon' to the beginning of the grocery list
-
-groceryList.unshift('Bread', 'Salmon');
-console.log(groceryList);
-
-// Pick up 'Egg', 'Chocolate', 'Eggplant, and 'Rice'
-
-const pickFourItems = groceryList.slice(2, 6);
-
-console.log(pickFourItems);
-console.log(`The answer should be: ['Egg', 'Chocolate', 'Eggplant', 'Rice']`);
-
+console.log(betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75));
